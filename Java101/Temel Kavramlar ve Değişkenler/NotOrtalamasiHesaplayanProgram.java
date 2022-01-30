f1a0a1538ab62a01b7f6cf16b257ecd6263344fd
Batuhan Şen <59296529+batuhansenn@users.noreply.github.com>
2022-01-30 05:23:41 +0300

    Add files via upload

---
+++ Java101/Temel Kavramlar ve Değişkenler/NotOrtalamasiHesaplayanProgram.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class NotOrtalamasiHesaplayanProgram {
+    public static void main(String[] args) {
+        int matematik, fizik, kimya, turkce, tarih, muzik;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Matematik Notunuz : ");
+        matematik = inp.nextInt();
+
+        System.out.print("Fizik Notunuz : ");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya Notunuz : ");
+        kimya = inp.nextInt();
+
+        System.out.print("Turkce Notunuz : ");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih Notunuz : ");
+        tarih = inp.nextInt();
+
+        System.out.print("Muzik Notunuz : ");
+        muzik = inp.nextInt();
+
+        int toplam = (matematik + fizik + kimya + turkce + tarih + muzik);
+        double sonuc = toplam / 6.0;
+        System.out.println("Not ortalamanız : " + sonuc);
+
+        System.out.println(sonuc > 60 ? "Sınıfı Geçti" : "Sınıfta Kaldınız");
+    }
+}
