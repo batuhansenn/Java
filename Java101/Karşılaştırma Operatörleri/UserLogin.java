e9afc6ec6c49ecf1e630abb83d3ac1269259a729
Batuhan Şen <59296529+batuhansenn@users.noreply.github.com>
2022-01-31 04:01:38 +0300

    Add files via upload

---
+++ Java101/Karşılaştırma Operatörleri/UserLogin.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class UserLogin {
+    public static void main(String[] args) {
+        String userName, password;
+        int control = 0, cont;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your Username : ");
+        userName = input.nextLine();
+
+        System.out.print("Enter your Password : ");
+        password = input.nextLine();
+
+        if (userName.equals("Batuhan") && password.equals("java123")) {
+            System.out.println("You have entered");
+        } else {
+            System.out.println("Wrong Username or Password ");
+            control = 1;
+        }
+
+        if (control == 1) {
+            System.out.println("Would you like to reset your password ?");
+            System.out.println("1.yes");
+            System.out.println("2.no");
+            cont = input.nextInt();
+            input.nextLine();
+
+            switch (cont) {
+                case 1:
+                    System.out.println("Please, Enter your new password");
+                    password = input.nextLine();
+                    if (password.equals("java123")) {
+                        System.out.println("Password couldn't be generated. Please, try another password");
+                    } else {
+                        System.out.println("New password is generated");
+                    }
+                    break;
+                case 2:
+                    System.out.println("Please, try to login again");
+                    break;
+                default:
+                    System.out.println("You have entered invalid character, try 1 or 2");
+                    break;
+            }
+        }
+    }
+
+}
